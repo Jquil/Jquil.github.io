c8745041a82835faf158080cf27641b556fcb960
Jquil <59355564+Jquil@users.noreply.github.com>
2021-06-14 01:54:51 -0500

    Add files via upload

---
+++ static/data/question.js
@@ -0,0 +1,12 @@
+window.question = {
+    "data":[
+        {
+            "id":1,
+            "tag":"Windows"
+        },
+        {
+            "id":2,
+            "tag":"Mac"
+        }
+    ]
+}
