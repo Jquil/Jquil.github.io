56a0c769ae5e1fc69990918785e0b876010104ab
Jqwong <59355564+Jquil@users.noreply.github.com>
2022-02-08 14:55:08 +0800

    Delete p/novel/static/js directory

--- p/novel/static/js/4.2fb7bbdd60f7383a0943.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([4],{j7e0:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var l={render:function(){var e=this.$createElement;return(this._self._c||e)("div",[this._v("200")])},staticRenderFns:[]},r=n("VU/8")(null,l,!1,null,null,null);t.default=r.exports}});
-//# sourceMappingURL=4.2fb7bbdd60f7383a0943.js.map
