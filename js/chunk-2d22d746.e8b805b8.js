26d0e0129714be084067ac4575f8c57f7a2080fa
Jqwong <59355564+Jquil@users.noreply.github.com>
2022-04-05 11:03:42 +0800

    Delete js directory

--- js/chunk-2d22d746.e8b805b8.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(n,e,t){"use strict";t.r(e);var u=t("7a23");function c(n,e,t,c,o,r){return Object(u["r"])(),Object(u["e"])("div",null," About ")}var o={name:"About"},r=t("6b0d"),d=t.n(r);const a=d()(o,[["render",c]]);e["default"]=a}}]);
-//# sourceMappingURL=chunk-2d22d746.e8b805b8.js.map
