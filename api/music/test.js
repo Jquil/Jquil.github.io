9ca66a6f8f6d9c307de6a4f3edc4fadc711494fa
Jqwong <59355564+Jquil@users.noreply.github.com>
2023-11-25 15:43:26 +0800

    Update test.js

--- api/music/test.js
+++ api/music/test.js
@@ -7,5 +7,11 @@ function add(a,b){
 }
 
 function a() {
-  return fetch("https://jquil.github.io/api/music/test.js?t=1700893076392")
+  var request = new XMLHttpRequest();
+  request.open("GET", "https://jquil.github.io/api/music/test.js?t=1700893076392", false);
+  request.send(null);
+  if (request.status === 200) {
+      return (request.responseText);
+  }
+  return ""
 }
